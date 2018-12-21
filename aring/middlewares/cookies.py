12e26d5603e0e914452a39d1a3120983784775cc
Peter Peter <dev.peterpeter5@gmailcom>
2018-12-21 07:57:46 +0100

    :cookies: expires can now be an int/float or a string

--- aring/middlewares/cookies.py
+++ aring/middlewares/cookies.py
@@ -1,14 +1,24 @@
 from http.cookies import SimpleCookie, CookieError, Morsel
 from urllib.parse import quote
+from wsgiref.handlers import format_date_time
+
 
 def make_morsel_from_description(name, descr):
     m = Morsel()
     value = str(descr.pop("value"))
     m.set(name, value, quote(value))
     for option, value in descr.items():
-        m[option] = value
+        m[option] = value if option != "expires" else format_expiration_date(value)
     return m
 
+
+def format_expiration_date(expires):
+    if isinstance(expires, str):
+        return expires
+    elif isinstance(expires, (int, float)):
+        return format_date_time(expires)
+    else:
+        raise TypeError(f"Dont know how to convert type <{type(expires)}> to time-header!")
 
 class CookieSerializer:
     def __init__(self, morsels):
@@ -32,6 +42,7 @@ def wrap_cookies(next_middleware):
 
         request["cookies"] = cookies        
         response = await next_middleware(handler, request)
+        expires = response.get("cookies", {}).get("expires")
         
         response_cookies = CookieSerializer(
             make_morsel_from_description(name, descr) 
--- test/test_cookies.py
+++ test/test_cookies.py
@@ -16,6 +16,16 @@ cookie_values = {
     "apple": {"value": "quick"}
 }
 
+@pytest.fixture
+def cookie_handler():
+    async def noop(request):
+        return {
+            "request_cookies": request.get("cookies"),
+            "cookies": {"a": {"value": "tasty_tomato"}, "b": {"value": 12, "secure": True}}
+        }
+    return apply_middleware(wrap_cookies)(noop)
+
+
 def test_header_serializer_with_headers_and_cookies():
     response = {
         "headers": {"content-type": "text/plain", "Y-X": "123"},
@@ -48,15 +58,6 @@ def test_cookie_serializer():
         }
 
 
-@pytest.fixture
-def cookie_handler():
-    async def noop(request):
-        return {
-            "request_cookies": request.get("cookies"),
-            "cookies": {"a": {"value": "tasty_tomato"}, "b": {"value": 12, "secure": True}}
-        }
-    return apply_middleware(wrap_cookies)(noop)
-
 @pytest.mark.asyncio
 async def test_cookie_header_get_parsed_to_dict(cookie_handler):
     response = await cookie_handler({"headers": {"cookie": "yummy_cookie=choco; tasty_cookie=strawberry"}})
@@ -72,3 +73,29 @@ async def test_cookie_response_dict_becomes_cookie_serializer(cookie_handler):
     assert set(values) == {
         "a=tasty_tomato", "b=12; Secure"
     }
+
+
+@pytest.mark.asyncio
+async def test_expires_date_can_be_given_as_string_and_will_be_used_as_such():
+    async def expires_string_cookie(req):
+        return {"cookies": {"s": {"value": 3.4, "expires": "nonsense-string"}}}
+    http_handler = apply_middleware(wrap_cookies)(expires_string_cookie)
+    response = await http_handler({})
+    values = list(map(lambda x: x[1], response["cookies"].items()))
+    assert len(values) == 1
+    assert values == [
+        "s=3.4; expires=nonsense-string"
+    ]
+
+
+@pytest.mark.asyncio
+async def test_expires_date_as_number_will_be_converted_to_string():
+    async def expires_int_float_cookie(req):
+        return {"cookies": {"s": {"value": 3.4, "expires": 1545335438.5059335}}}
+    http_handler = apply_middleware(wrap_cookies)(expires_int_float_cookie)
+    response = await http_handler({})
+    values = list(map(lambda x: x[1], response["cookies"].items()))
+    assert len(values) == 1
+    assert values == [
+        "s=3.4; expires=Thu, 20 Dec 2018 19:50:38 GMT"
+    ]
